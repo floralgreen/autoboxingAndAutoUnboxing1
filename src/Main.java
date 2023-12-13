e76099633f3f4f9faf8c7598be691d44e116a39a
floralgreen <carducci.federico20@gmail.com>
2023-12-13 21:04:07 +0100

    Esercizio Completato + aggiunti commenti esplicativi

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,11 @@ public class Main {
         int numInt1 = 10;
         int numInt2 = 15;
 
-        double numDouble1 = 50.0;
-        double numDouble2 = 15.5;
-
         char carattere = 'c';
 
         //utilizzo i tipi primitivi su i metodi che richiedono le classi Wrapper come parametri
+        //facendo così autoboxing
         operations.stampaSommaInteger(numInt1, numInt2);
-        operations.stampaSommaDoubleWrapper(numDouble1, numDouble2);
         operations.stampaCarattereWrapper(carattere);
 
 
@@ -22,15 +19,20 @@ public class Main {
         Integer integer1 = 5;
         Integer integer2 = 7;
 
-        Double numDoubleWrap1 = 5.5;
-        Double numDoubleWrap2 = 4.2;
-
         Character charWrap = 'f';
 
         //utilizzo i tipi wrapper su i metodi che richiedono primitivi come parametri
+        //facendo così unboxing
         operations.stampaSommaInt(integer1, integer2);
-        operations.stampaSommaDouble(numDoubleWrap1, numDoubleWrap2);
         operations.stampaCaratterePrimitive(charWrap);
+
+        //autoboxing tramite assegnazione
+        double numDouble1 = 50.0;
+        Double numDoubleWrap1 = numDouble1;
+
+        //Unboxing treamite assegnazione
+        Double numDoubleWrap2 = 4.2;
+        double numDouble2 = numDoubleWrap2;
 
 
 
--- src/Operations.java
+++ src/Operations.java
@@ -4,20 +4,12 @@ public class Operations {
         int somma = num1 + num2;
         System.out.println("La somma dei 2 int è: " + somma);
     }
-    public void stampaSommaDouble(double num1, double num2) {
-        double somma = num1 + num2;
-        System.out.println("La somma dei 2 int è: " + somma);
-    }
     public void stampaCaratterePrimitive(char carattere) {
         System.out.println("Il carattere è: '" + carattere + "'");
     }
     public void stampaSommaInteger(Integer num1, Integer num2){
         Integer somma = num1 + num2;
         System.out.println("La somma dei 2 Integer è: " + somma);
-    }
-    public void stampaSommaDoubleWrapper(Double num1, Double num2) {
-        Double somma = num1 + num2;
-        System.out.println("La somma dei 2 int è: " + somma);
     }
     public void stampaCarattereWrapper(Character carattere) {
         System.out.println("Il carattere è: '" + carattere + "'");
